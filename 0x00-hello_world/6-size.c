46f447c5901508de2198085091ef077fce30991c
Ruth Boyle <boyleruth764@gmail.com>
2023-04-23 19:29:02 -0700

    Updated low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main -Prints the size of various typesa based on 
+ * main - Prints the size of various typesa based on 
  * the computer it is compiled and run on..
  * Return: Always 0.
  */
@@ -9,6 +10,7 @@ int main(void)
 
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
