51fd2c0011fc4303a7939a4a4c539e8c9eccc97c
Ruth Boyle <boyleruth764@gmail.com>
2023-04-23 19:22:54 -0700

    Updated low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various typesa based on 
+ * main -Prints the size of various typesa based on 
  * the computer it is compiled and run on..
  * Return: Always 0.
  */
