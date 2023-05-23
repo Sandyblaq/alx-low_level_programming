164542cfac8ebc78d57d55b103fb0ff77695a964
Ruth Boyle <boyleruth764@gmail.com>
2023-05-22 20:38:12 -0700

    1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * int_dog - Initializes a dog structure
+ * init_dog - Initializes a dog structure
  * @d: A dog structure.
  * @name: The name of the dog.
  * @age: The age of the dog.
