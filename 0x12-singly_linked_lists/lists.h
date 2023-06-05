0d3659ce6e0f1e85bbfecf7779fecdd7e1ff038b
Sandyblaq <boyleruth764@gmail.com>
2023-06-04 19:29:04 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,8 @@
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of the next node
+ * @len: length of the string
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for alx project
